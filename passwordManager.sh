f245202f95ac93bc98b9456cc58ac91b6d0d54b9
mahorobach <dokakao@gmail.com>
2025-02-17 13:14:15 +0900

    Step2

--- passwordManager.sh
+++ passwordManager.sh
@@ -1,9 +1,10 @@
 echo "パスワードマネージャへようこそ！"
+
 echo "サービス名を入力してください："
 read service
 echo "ユーザー名を入力してください"
 read user
 echo "パスワードを入力してください"
 read pass
-echo "$service:$user:$pass" >> step1.txt
+echo "$service:$user:$pass" >> pass.txt
 echo "Thank you!"
